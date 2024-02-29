a35cd3685d7e441a50542ff39227394fe33f1cab
Davi Mak <121460974+DaviMak@users.noreply.github.com>
2024-02-28 22:02:27 -0300

    Add files via upload
    continuacao do trabalho

---
+++ parte um/Entrega-seman-vel-main/src/main/java/app/entity/Livro.java
@@ -0,0 +1,68 @@
+package app.entity;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class Livro {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String issn;
+	private String titulo;
+	private String sinopse;
+	private int ano;
+	private int nrPaginas;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getIssn() {
+		return issn;
+	}
+	public void setIssn(String issn) {
+		this.issn = issn;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getSinopse() {
+		return sinopse;
+	}
+	public void setSinopse(String sinopse) {
+		this.sinopse = sinopse;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public int getNrPaginas() {
+		return nrPaginas;
+	}
+	public void setNrPaginas(int nrPaginas) {
+		this.nrPaginas = nrPaginas;
+	}
+	//teste
+}
